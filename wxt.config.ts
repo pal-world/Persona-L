8a243f990f52d963cd0e0fb4fbe0bba52507e462
DaeHan Kim <kjs50458281@gmail.com>
2025-02-27 14:30:05 +0900

    chore: Add auto-icons module and update project icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
+  modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
   manifest: {
     name: 'Persona-L',
     description: '현재 페이지의 내용을 작가의 마음으로 요약해주는 확장 프로그램',
     permissions: ['activeTab', 'storage', 'scripting'],
-    host_permissions: ['<all_urls>']
-  }
+    host_permissions: ['<all_urls>'],
+  },
 });
